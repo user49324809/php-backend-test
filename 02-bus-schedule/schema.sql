d7c42581ebccc34e322448240b8397ad34d81567
Natalia <inna6903zaharova@yandex.ru>
2026-09-21 21:48:52 +0300

    Add stops table

--- 02-bus-schedule/schema.sql
+++ 02-bus-schedule/schema.sql
@@ -2,3 +2,13 @@ CREATE TABLE cities (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL UNIQUE
 );
+
+CREATE TABLE stops (
+    id SERIAL PRIMARY KEY,
+    city_id INT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+
+    CONSTRAINT fk_stops_city
+        FOREIGN KEY (city_id)
+        REFERENCES cities(id)
+);

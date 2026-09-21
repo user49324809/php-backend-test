ad0e0a1b60bc0a2454cd16da3769936402f2a56f
Natalia <inna6903zaharova@yandex.ru>
2026-09-21 21:33:55 +0300

    Implement palindrome comparison

--- 01-palindrome/test.php
+++ 01-palindrome/test.php
@@ -4,6 +4,15 @@ require_once __DIR__ . '/palindrome.php';
 
 $tests = [
     ['level', true],
+    ['Level', true],
     ['hello', false],
+
     ['топот', true],
+    ['А роза упала на лапу Азора', true],
+
+    ['12321', true],
+    ['12345', false],
+
+    ['a', true],
+    ['', true],
 ];

0e8e091fb106ef4a026162f4769c41161824dfde
Natalia <inna6903zaharova@yandex.ru>
2026-09-21 15:04:53 +0300

    Normalize palindrome input

--- 01-palindrome/palindrome.php
+++ 01-palindrome/palindrome.php
@@ -5,4 +5,9 @@ declare(strict_types=1);
 function isPalindrome(string $text): bool
 {
     $normalized = mb_strtolower($text, 'UTF-8');
+    $normalized = preg_replace(
+        '/[^\p{L}\p{N}+/u',
+        '',
+        $normalized
+    );
 }

fc32135ed94eca63fbabd2595ef345357da99cd7
Natalia <inna6903zaharova@yandex.ru>
2026-09-21 21:45:08 +0300

    create schema

--- 02-bus-schedule/schema.sql
+++ 02-bus-schedule/schema.sql
@@ -2,3 +2,9 @@ CREATE TABLE cities(
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL UNIQUE
 );
+INSERT INTO cities(name) VALUES
+('Казань'),
+('Уфа'),
+('Самара'),
+('Москва'),
+('Пермь');
